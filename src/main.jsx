07815cb80f5b46a9b38a2fbdf356b4c6c740b1fb
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-07-23 16:25:04 +0300

    fix file paths and imports for ChessBoard components

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import ChessBoard from '../ChessBoard/ChessBoard'
-import App from '../ChessBoard/App'
+import App from './ChessBoard/App'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <ChessBoard></ChessBoard>
   <App></App>
   </StrictMode>,
 )
