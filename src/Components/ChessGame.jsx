30990b780381e3d453c9d39cbf83e6bcc8a91bea
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-07-25 11:31:23 +0300

    add change that group member made in ChessGame.jsx

--- src/Components/ChessGame.jsx
+++ src/Components/ChessGame.jsx
@@ -3,15 +3,17 @@ import GameBoard from './GameBoard';
 import { getNextTurn, isValidTurn } from '../logic/turnManager';
 import './ChessBoard.css';
 
-export default function ChessGame() {
+function ChessGame() {
   const [currentTurn, setCurrentTurn] = useState('white');
 
   const handleMove = (fromSquare, toSquare, pieceColor) => {
-  
+    
     if (!isValidTurn(currentTurn, pieceColor)) {
       console.log("Not your turn!");
       return false;
     }
+
+    
     console.log(`${pieceColor} moves from ${fromSquare} to ${toSquare}`);
     setCurrentTurn(getNextTurn(currentTurn));
     return true;
@@ -23,3 +25,5 @@ export default function ChessGame() {
     </div>
   );
 }
+
+export default ChessGame;
